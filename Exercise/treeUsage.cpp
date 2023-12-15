7b3278ba4ee74bc749e7d05a56f9327d2cf2a536
JeongMin-98 <jeongmin981@gmail.com>
2023-12-15 14:04:38 +0900

    [add] tree structure

---
+++ Exercise/treeUsage.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jeong on 2023-12-14.
+//
+#include "binaryTree/binaryTree.hpp"
+#include <iostream>
+
+int main() {
+    binaryTree tree("root");
+    tree.addNode("root", "f1");
+
+    return 0;
+
+}
