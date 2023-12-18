9bee6dace361a9451ac83d5ca376e823562e12f0
JeongMin-98 <jeongmin981@gmail.com>
2023-12-18 15:48:09 +0900

    [fix] binarySearchTree

--- libs/binaryTree/include/binaryTree/binaryTree.hpp
+++ libs/binaryTree/include/binaryTree/binaryTree.hpp
@@ -8,19 +8,24 @@
 #include<iostream>
 #include<queue>
 
+template<typename T>
 struct node {
-    std::string position;
-    node *first;
-    node *second;
+    T data;
+    node *left;
+    node *right;
 
-    node(std::string pos) : position(pos), first(nullptr), second(nullptr) {}
+    node(T pos) : data(pos), left(nullptr), right(nullptr) {}
 };
+
 
 class binaryTree {
 public:
-    using nodePtr = node *;
+    using nodePtr = node<std::string> *;
 private:
     nodePtr root;
+
+    bool removeNode(nodePtr curNode);
+
 public:
     binaryTree() : root(nullptr) {}
 
@@ -30,17 +35,126 @@ public:
 
     nodePtr getRoot();
 
-    bool removeNode(node *curNode);
 
-    nodePtr find(node *curNode, const std::string value);
+    nodePtr find(nodePtr curNode, const std::string value);
 
-    bool addNode(const std::string senior, const std::string junior);
+    void preOrder(nodePtr start);
 
-    void preOrder(node *start);
+    void inOrder(nodePtr start);
 
-    void inOrder(node *start);
+    void postOrder(nodePtr start);
 
-    void postOrder(node *start);
+    bool addNode(const std::string &senior, const std::string &junior);
 };
+
+template<typename T>
+class BinarySearchTree {
+public:
+    using Node = node<T>;
+    using nodePtr = Node *;
+private:
+    nodePtr root = nullptr;
+
+public:
+
+    ~BinarySearchTree<T>();
+
+    BinarySearchTree<T>::nodePtr find(T value);
+
+    void insert(T value);
+
+    void deleteNode(nodePtr curNode);
+
+private:
+    nodePtr find_impl(nodePtr curNode, T value);
+
+    void insert_impl(nodePtr curNode, T value);
+
+    void deleteNodeImpl(nodePtr curNode);
+};
+
+template<typename T>
+BinarySearchTree<T>::~BinarySearchTree<T>() {
+    deleteNode(root);
+}
+
+template<typename T>
+typename BinarySearchTree<T>::nodePtr BinarySearchTree<T>::find_impl(nodePtr curNode, T value) {
+    if (curNode == nullptr) return nullptr;
+
+    if (value == curNode->data) {
+        std::cout << "find value" << std::endl;
+        return curNode;
+    }
+
+    if (value < curNode->data) {
+        std::cout << "your value is smaller than curNode data " << curNode->data << " So, step left node" << std::endl;
+        return find_impl(curNode->left, value);
+    } else {
+        std::cout << "your value is bigger than curNode data " << curNode->data << " So, step right node" << std::endl;
+        return find_impl(curNode->right, value);
+    }
+}
+
+template<typename T>
+typename BinarySearchTree<T>::nodePtr BinarySearchTree<T>::find(T value) {
+    return find_impl(root, value);
+}
+
+template<typename T>
+void BinarySearchTree<T>::insert_impl(BinarySearchTree::nodePtr curNode, T value) {
+    if (value < curNode->data) {
+        if (curNode->left == nullptr) {
+            curNode->left = new Node(value);
+            return;
+        } else {
+            insert_impl(curNode->left, value);
+        }
+    } else {
+        if (curNode->right == nullptr) {
+            curNode->right = new Node(value);
+            return;
+        } else {
+            insert_impl(curNode->right, value);
+        }
+    }
+}
+
+template<typename T>
+void BinarySearchTree<T>::insert(T value) {
+    if (root == nullptr) {
+        root = new Node(value);
+    } else {
+        insert_impl(root, value);
+    }
+}
+
+template<typename T>
+void BinarySearchTree<T>::deleteNodeImpl(BinarySearchTree::nodePtr curNode) {
+    if (curNode->left != nullptr) {
+        deleteNodeImpl(curNode->left);
+        curNode->left = nullptr;
+    }
+    if (curNode->right != nullptr) {
+        deleteNodeImpl(curNode->right);
+        curNode->right = nullptr;
+    }
+
+    if (curNode->left == nullptr && curNode->right == nullptr) {
+        std::cout << "curNode [" << curNode->data << "] deconstructed" << std::endl;
+        auto temp = curNode;
+        temp->left = nullptr;
+        temp->right = nullptr;
+        delete temp;
+        return;
+    }
+}
+
+template<typename T>
+void BinarySearchTree<T>::deleteNode(BinarySearchTree::nodePtr curNode) {
+    if (curNode != nullptr){
+        deleteNodeImpl(curNode);
+    }
+}
 
 #endif //CPPLEARN_BINARYTREE_HPP
--- libs/binaryTree/src/binaryTree.cpp
+++ libs/binaryTree/src/binaryTree.cpp
@@ -4,8 +4,8 @@
 
 #include "binaryTree.hpp"
 
-binaryTree::binaryTree(const std::string pos) {
-    root = new node(pos);
+binaryTree::binaryTree(const std::string value) {
+    root = new node<std::string>(value);
 }
 
 binaryTree::~binaryTree() {
@@ -13,83 +13,84 @@ binaryTree::~binaryTree() {
     removeNode(root);
 }
 
-bool binaryTree::removeNode(node *curNode) {
+bool binaryTree::removeNode(nodePtr curNode) {
     if (curNode == nullptr) {
         return false;
     }
 
-    if (curNode->first != nullptr) {
-        removeNode(curNode->first);
-        curNode->first = nullptr;
+    if (curNode->left != nullptr) {
+        removeNode(curNode->left);
+        curNode->left = nullptr;
     }
-    if (curNode->second != nullptr) {
-        removeNode(curNode->second);
-        curNode->second = nullptr;
+    if (curNode->right != nullptr) {
+        removeNode(curNode->right);
+        curNode->right = nullptr;
     }
 
-    if (curNode->first == nullptr && curNode->second == nullptr) {
-        std::cout << "curNode [" << curNode->position << "] deconstructed" << std::endl;
+    if (curNode->left == nullptr && curNode->right == nullptr) {
+        std::cout << "curNode [" << curNode->data << "] deconstructed" << std::endl;
         auto temp = curNode;
-        temp->first = nullptr;
-        temp->second = nullptr;
+        temp->left = nullptr;
+        temp->right = nullptr;
         delete temp;
         return true;
     }
+    return false;
 }
 
-binaryTree::nodePtr binaryTree::find(node *curNode, const std::string value) {
+binaryTree::nodePtr binaryTree::find(nodePtr curNode, const std::string value) {
     if (curNode == nullptr)
         return nullptr;
-    if (curNode->position == value) {
+    if (curNode->data == value) {
         return curNode;
     }
-    auto firstNode = find(curNode->first, value);
+    auto firstNode = find(curNode->left, value);
     if (firstNode != nullptr) return firstNode;
-    return find(curNode->second, value);
+    return find(curNode->right, value);
 }
 
-bool binaryTree::addNode(const std::string senior, const std::string junior) {
+bool binaryTree::addNode(const std::string &senior, const std::string &junior) {
     auto seniorNode = find(this->root, senior);
 
     if (seniorNode == nullptr) {
         std::cout << senior << "를 찾을 수 없습니다. " << std::endl;
         return false;
     }
-    if (seniorNode->first && seniorNode->second) {
+    if (seniorNode->left && seniorNode->right) {
         std::cout << senior << "아래에 가득차서 등록할 수 없습니다. " << std::endl;
         return false;
     }
-    if (seniorNode->first == nullptr) {
-        seniorNode->first = new node(junior);
+    if (seniorNode->left == nullptr) {
+        seniorNode->left = new node<std::string>(junior);
     } else {
-        seniorNode->second = new node(junior);
+        seniorNode->right = new node<std::string>(junior);
     }
     std::cout << "[" << senior << "] 아래에 [" << junior << "] 를(을) 추가했습니다. " << std::endl;
     return true;
 }
 
-void binaryTree::preOrder(node *start) {
+void binaryTree::preOrder(nodePtr start) {
     if (start == nullptr) return;
 
-    std::cout << start->position << ", ";
-    preOrder(start->first);
-    preOrder(start->second);
+    std::cout << start->data << ", ";
+    preOrder(start->left);
+    preOrder(start->right);
 }
 
-void binaryTree::inOrder(node *start) {
+void binaryTree::inOrder(nodePtr start) {
     if (start == nullptr) return;
 
-    inOrder(start->first);
-    std::cout << start->position << ", ";
-    inOrder(start->second);
+    inOrder(start->left);
+    std::cout << start->data << ", ";
+    inOrder(start->right);
 }
 
-void binaryTree::postOrder(node *start) {
+void binaryTree::postOrder(nodePtr start) {
     if (start == nullptr) return;
 
-    postOrder(start->first);
-    postOrder(start->second);
-    std::cout << start->position << ", ";
+    postOrder(start->left);
+    postOrder(start->right);
+    std::cout << start->data << ", ";
 }
 
 binaryTree::nodePtr binaryTree::getRoot() {
